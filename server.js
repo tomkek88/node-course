2a8b1709e7b33ed7512146e29ed7c7fb4e9bd0da
Tomasz Brazkiewicz <tom.brazkiewicz@gmail.com>
2018-10-25 13:12:26 +0200

    listen log

--- server.js
+++ server.js
@@ -65,4 +65,6 @@ app.get("/bad", (req, res) => {
 
 // ============ End of Routes ===============
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
